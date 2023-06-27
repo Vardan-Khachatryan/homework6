ee26d89917f47ebaf27a57d498713c9b657bb53c
Vardan Khachatryan <134618862+Vardan-Khachatryan@users.noreply.github.com>
2023-06-27 18:32:59 +0400

    commit for homework6

---
+++ src/button/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Buttonadd.css";
+
+export const Button = () => {
+  return (
+    <div className="Buttonadd">
+      <button>Hello</button>
+    </div>
+  );
+};
---
+++ src/button/Deletebutton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Deletebutton.css";
+
+export const Deletebutton = () => {
+  return (
+    <div className="deletebutton">
+      <button>Delete</button>
+    </div>
+  );
+};
